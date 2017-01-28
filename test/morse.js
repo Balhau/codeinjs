689ab645f9e927d8e6e72c56ceb96913c1c23393
Balhau <balhau@balhau.net>
2017-01-28 20:22:07 +0000

    Added number codes and test

--- test/morse.js
+++ test/morse.js
@@ -11,4 +11,12 @@ describe("Testing morse code library",function(){
     var m = new morse();
     expect(m.fromMorse(m.toMorse("hello world"))).to.be.equal("hello world");
   })
+
+  it("Numbers conversion",function(){
+    var m = new morse();
+    var encoded = m.toMorse("1945")
+    var decoded = m.fromMorse(encoded)
+    expect(encoded).to.be.equal("*---- ----* ****- *****");
+    expect(decoded).to.be.equal("1945")
+  });
 });
